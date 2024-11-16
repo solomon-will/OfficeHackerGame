f07b63389e4fc08060b4093f7e38a916b9015411
Christian Rubio <98237169+c-rubio@users.noreply.github.com>
2024-11-15 23:38:35 -0500

    Puzzle Draft

--- Assets/Mini Toon Office/Scripts/MouseClick.cs
+++ Assets/Mini Toon Office/Scripts/MouseClick.cs
@@ -7,9 +7,14 @@ public class MouseClick : MonoBehaviour
     public TextMeshPro computerText;
     public Slider stressProgress;
     public int stressImpact;
+    public GameObject[] puzzle;
 
     void OnMouseDown() {
         computerText.text = "Login Successful.";
         stressProgress.value += stressImpact;
+        foreach (GameObject block in puzzle) {
+            block.transform.position += new Vector3(0, 0, 0.03f);
+        }
+
     }
 }
