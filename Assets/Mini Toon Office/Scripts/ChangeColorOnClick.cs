0e15ebb08fe79701fdb99a29b68ad50de910e278
solomon-will <solomonwill13@gmail.com>
2024-11-15 23:32:33 -0500

    added scripts to objects

--- Assets/Mini Toon Office/Scripts/ChangeColorOnClick.cs
+++ Assets/Mini Toon Office/Scripts/ChangeColorOnClick.cs
@@ -1,8 +1,12 @@
 using UnityEngine;
+using UnityEngine.UI;
+
 
 public class ChangeColorOnClick : MonoBehaviour
 {
     public Color newColor = Color.red;
+    public Slider stressProgress;
+    public int stressImpact;
     
     public Vector3 newScale = new Vector3(2f, 2f, 2f); 
     void OnMouseDown()
@@ -17,5 +21,8 @@ public class ChangeColorOnClick : MonoBehaviour
 
         transform.localScale = newScale;
         Debug.Log("scale changed"); 
+
+        stressProgress.value += stressImpact;
+
     }
 }
